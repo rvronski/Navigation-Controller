18198fa1869c179b0993751e3a5e270d63419fa2
rvronski <rvronski@gmail.com>
2022-07-19 17:29:41 +0300

    create InfoVC

---
+++ Navigation/InfoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InfoViewController.swift
+//  Navigation
+//
+//  Created by ROMAN VRONSKY on 19.07.2022.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .tintColor
+    }
+    
+    lazy var myButton: UIButton
+}
--- Navigation/PostViewController.swift
+++ Navigation/PostViewController.swift
@@ -9,13 +9,20 @@ import UIKit
 
 class PostViewController: UIViewController{
     let vc = FeedViewController()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .lightGray
         self.navigationItem.title = vc.post.title
+        var button = UIBarButtonItem(barButtonSystemItem: .play, target: self, action: #selector(modalMenu))
+        navigationItem.rightBarButtonItem = button
     }
     
     
     
+    @objc func modalMenu() {
+        let vc = InfoViewController()
+        vc.modalPresentationStyle = .automatic
+        self.present(vc, animated: true)
+    }
 }
