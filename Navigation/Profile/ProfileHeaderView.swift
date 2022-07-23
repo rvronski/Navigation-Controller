9abf95a542584d4e8ed59582b3ff2c7360fbdf4f
rvronski <rvronski@gmail.com>
2022-07-23 19:47:14 +0300

    create ProfileHeaderView

---
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileView.swift
+//  Navigation
+//
+//  Created by ROMAN VRONSKY on 23.07.2022.
+//
+
+import UIKit
+
+class ProfileHeaderView: UIView {
+    
+}
--- Navigation/ProfileViewController.swift
+++ Navigation/ProfileViewController.swift
@@ -8,9 +8,16 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-    
+    let profileHeader = ProfileHeaderView()
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .white
+        self.view.backgroundColor = .lightGray
+        self.view.addSubview(profileHeader)
+        
+        
+        func viewWillLayoutSubviews() {
+            
+        
     }
+}
 }
