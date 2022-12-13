48dfbeaf232d20b13055cb7073a9b848e832ff8c
rvronski <rvronski@gmail.com>
2022-12-13 22:38:53 +0500

    add func to load orbital period

--- Navigation/Modules/Info/InfoNetworkManager.swift
+++ Navigation/Modules/Info/InfoNetworkManager.swift
@@ -7,9 +7,14 @@
 
 import Foundation
 
+struct Planet: Decodable {
+    var orbital_period: String
+}
+
+
 struct InfoNetworkManager {
     
-    static func request(completion: ((_ textTitle: [String]?) -> Void)?) {
+    static func loadText(completion: ((_ textTitle: [String]?) -> Void)?) {
         guard let url = URL(string: "https://jsonplaceholder.typicode.com/todos/" ) else { return }
         let session = URLSession(configuration: .default)
         let task = session.dataTask(with: url) { data, response, error in
@@ -59,4 +64,48 @@ struct InfoNetworkManager {
         
         task.resume()
     }
+    
+    
+    
+    
 }
+
+func loadOrbitalPeriod(completion: ((_ orbital_period: String?) -> Void)?) {
+    guard let url = URL(string: "https://swapi.dev/api/planets/1" ) else { return }
+    let session = URLSession(configuration: .default)
+    let task = session.dataTask(with: url) { data, response, error in
+        if let error {
+            print(error.localizedDescription)
+            completion?(nil)
+            return
+        }
+        let statusCode = (response as? HTTPURLResponse)?.statusCode
+        let headerFields = (response as? HTTPURLResponse)?.allHeaderFields
+        print("🍏 Statys Code = \(String(describing: statusCode))")
+        print("🍎 Header Fields = \(String(describing: headerFields))")
+        if statusCode != 200 {
+            print("Status Code = \(String(describing: statusCode))")
+            completion?(nil)
+            return
+        }
+        guard let data else {
+            print("data = nil")
+            completion?(nil)
+            return
+        }
+        do {
+           let period = try JSONDecoder().decode(Planet.self, from: data)
+            let orbital_Period = period.orbital_period
+            completion?(orbital_Period)
+        } catch {
+            completion?(nil)
+            print(error)
+        }
+        
+    }
+    
+    task.resume()
+}
+
+
+
--- Navigation/Modules/Info/InfoViewController.swift
+++ Navigation/Modules/Info/InfoViewController.swift
@@ -17,23 +17,36 @@ class InfoViewController: UIViewController {
     private func setupView() {
         self.view.backgroundColor = .white
         self.view.addSubview(self.myButton)
-        self.view.addSubview(self.infoLabel)
+        self.view.addSubview(self.infoTextLabel)
+        self.view.addSubview(self.planetButton)
+        self.view.addSubview(self.infoPlanetLabel)
         
         NSLayoutConstraint.activate([
-            self.infoLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
-            self.infoLabel.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 16),
-            self.infoLabel.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -16),
-            self.infoLabel.heightAnchor.constraint(equalToConstant: 200),
+            self.infoTextLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            self.infoTextLabel.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 16),
+            self.infoTextLabel.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -16),
+            self.infoTextLabel.heightAnchor.constraint(equalToConstant: 100),
             
-            self.myButton.topAnchor.constraint(equalTo: self.infoLabel.bottomAnchor, constant: 16),
+            self.infoPlanetLabel.bottomAnchor.constraint(equalTo: self.infoTextLabel.topAnchor),
+            self.infoPlanetLabel.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 16),
+            self.infoPlanetLabel.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -16),
+            self.infoPlanetLabel.heightAnchor.constraint(equalToConstant: 50),
+            
+            self.myButton.topAnchor.constraint(equalTo: self.infoTextLabel.bottomAnchor, constant: 16),
             self.myButton.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 16),
             self.myButton.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -16),
-            self.myButton.heightAnchor.constraint(equalToConstant: 50)
+            self.myButton.heightAnchor.constraint(equalToConstant: 50),
+            
+            self.planetButton.topAnchor.constraint(equalTo: self.myButton.bottomAnchor, constant: 16),
+            self.planetButton.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 16),
+            self.planetButton.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -16),
+            self.planetButton.heightAnchor.constraint(equalToConstant: 50)
+            
             
         ])
     }
     
-    private lazy var infoLabel: UILabel = {
+    private lazy var infoTextLabel: UILabel = {
         let infoLabel = UILabel()
         infoLabel.lineBreakMode = .byWordWrapping
         infoLabel.numberOfLines = 0
@@ -42,26 +55,54 @@ class InfoViewController: UIViewController {
         return infoLabel
     }()
     
-   private lazy var myButton: UIButton = {
+    private lazy var infoPlanetLabel: UILabel = {
+        let infoLabel = UILabel()
+        infoLabel.textAlignment = .center
+        infoLabel.translatesAutoresizingMaskIntoConstraints = false
+        return infoLabel
+    }()
+    
+    private lazy var myButton: UIButton = {
         let button = UIButton()
-       button.translatesAutoresizingMaskIntoConstraints = false
+        button.translatesAutoresizingMaskIntoConstraints = false
         button.layer.cornerRadius = 10
         button.layer.borderWidth = 2.0
         button.layer.borderColor = UIColor.white.cgColor
-       button.backgroundColor = .link
-        button.setTitle("Push", for: .normal)
+        button.backgroundColor = .link
+        button.setTitle("Text", for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.addTarget(self, action: #selector(self.showText), for: .touchUpInside)
         return button
     }()
+    private lazy var planetButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 10
+        button.layer.borderWidth = 2.0
+        button.layer.borderColor = UIColor.white.cgColor
+        button.backgroundColor = .link
+        button.setTitle("Orbital Period", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(self.showPlanetOrbital), for: .touchUpInside)
+        return button
+    }()
     
     @objc func showText() {
-       
-            InfoNetworkManager.request { [weak self] textArray in
-                DispatchQueue.main.async {
-                    self?.infoLabel.text = textArray?.randomElement()
-                }
-            }
         
+        InfoNetworkManager.loadText { [weak self] textArray in
+            DispatchQueue.main.async {
+                self?.infoTextLabel.text = textArray?.randomElement()
+            }
+        }
+        
+    }
+    
+    @objc func showPlanetOrbital() {
+        
+        loadOrbitalPeriod { orbital_period in
+            DispatchQueue.main.async {
+                self.infoPlanetLabel.text = "Орбитальный период - \(orbital_period ?? "0")"
+            }
+        }
     }
 }
