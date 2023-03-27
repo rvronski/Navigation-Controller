eb8cda15af993f3832d2085e7612c8782182b2b1
rvronski <rvronski@gmail.com>
2023-03-27 20:04:44 +0500

    change in color extension

--- Navigation/Extension/Color.swift
+++ Navigation/Extension/Color.swift
@@ -11,8 +11,6 @@ import UIKit
 extension UIColor {
     
     static func createColor(light: UIColor, dark: UIColor) -> UIColor {
-        guard #available(iOS 13.0, *) else { return light }
-        
         return UIColor { (traitCollection) -> UIColor in
             return traitCollection.userInterfaceStyle == .light ? light : dark
         }
